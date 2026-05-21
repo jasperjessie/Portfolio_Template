e9a130f3ec86504d299547ff75af861785cf14d5
jasperjessie <JASPERJESSIE@GMAIL.COM>
2026-05-22 03:13:15 +0530

    Update NotionPageHeader.tsx

--- components/NotionPageHeader.tsx
+++ components/NotionPageHeader.tsx
@@ -67,13 +67,37 @@ export function NotionPageHeader({
                 )
               } else {
                 return (
-                  <components.Link
-                    href={link.url}
-                    key={index}
-                    className={cs(styles.navLink, 'breadcrumb', 'button')}
-                  >
-                    {link.title}
-                  </components.Link>
+                  //innovate//
+                  // <components.Link
+                  //   href={link.url}
+                  //   key={index}
+                  //   className={cs(styles.navLink, 'breadcrumb', 'button')}
+                  // >
+                  //   {link.title}
+                  // </components.Link>
+                  //innvocate//
+                  <a
+  href={link.url}
+  key={index}
+  className={cs(styles.navLink, 'breadcrumb', 'button')}
+  onClick={(e) => {
+    e.preventDefault()
+
+    const targetId = link.url?.replace('#', '')
+    const target = document.getElementById(targetId)
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      })
+
+      window.history.replaceState(null, '', ' ')
+    }
+  }}
+>
+  {link.title}
+</a>
+              //innovate
                 )
               }
             })
